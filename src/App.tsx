8eca29ccb1cab389f9dc3936e0d5d02b287cb3ec
Daniel <daniel.bencz@gmail.com>
2020-03-25 09:54:14 +0100

    basic styling, beginning with IDB

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// start interact with IDB
+let dbs;
+if ("indexedDB" in window && window.indexedDB !== undefined) {
+  dbs = window.indexedDB
+  console.log('starting')
+}
+else {
+  alert("No support for indexedDB")
+}
+
+
+const Note = () => {
+  return (
+    <div id="note">
+      <ul>
+        <li>
+          <h3>my note title</h3>
+          <p>my note body</p>
+          <button>Delete note</button>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+const Display = () => {
+  return (
+    <div id="display">
+      <h2>Notes</h2>
+      <Note></Note>
+    </div>
+  )
+}
+
+const Form = () => {
+  return (
+    <>
+      <h2>New Note</h2>
+      <input id="title" type="text"></input>
+      <input id="body" type="text"></input>
+      <button>Create new note</button>
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IndexedDB with React</h1>
+      <Display></Display>
+      <Form></Form>
     </div>
-  );
+  )
 }
 
 export default App;
