e8119f6e20a73e116914ea7c4473b9075bd4bc01
Daniel <daniel.bencz@gmail.com>
2020-03-26 14:06:40 +0100

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-import React from 'react';
+// https://visualstudiomagazine.com/articles/2016/08/30/storing-data-client-javascript-typescript.aspx
+// https://visualstudiomagazine.com/articles/2016/09/01/working-with-indexeddb.aspx
+
+import React, { useEffect } from 'react';
 import './App.css';
 
-if ("indexedDB" in window && window.indexedDB !== undefined) {
-  let dbs: IDBFactory = window.indexedDB
-  let dbName: string = 'pwa_notes_db'
-  let request: IDBOpenDBRequest
-  request = dbs.open(dbName, 1)
-
-  request.onupgradeneeded = (e: any) => {
-    let database: IDBDatabase
-    database = e.target.result
-    console.log('upgrade needed')
-  }
-}
-else {
-  alert("No support for indexedDB")
-}
-
-
-const Note = () => {
+const Note = (props: any) => {
   return (
     <div id="note">
       <ul>
         <li>
-          <h3>my note title</h3>
-          <p>my note body</p>
+          <h3>{props.title}</h3>
+          <p>{props.body}</p>
           <button>Delete note</button>
         </li>
       </ul>
@@ -36,12 +22,43 @@ const Display = () => {
   return (
     <div id="display">
       <h2>Notes</h2>
-      <Note></Note>
-    </div>
+      <Note title="my title" body="description"></Note>
+    </div >
   )
 }
 
 const Form = () => {
+
+  useEffect(() => {
+    if ("indexedDB" in window && window.indexedDB !== undefined) {
+      let idbf: IDBFactory = window.indexedDB
+      let dbName: string = 'pwa_notes_db'
+      let request: IDBOpenDBRequest
+      request = idbf.open(dbName, 3)
+
+      request.onupgradeneeded = (e: any) => {
+        console.log('upgrade needed')
+
+        let database: IDBDatabase
+        database = e.target.result
+
+        let objectStore: IDBObjectStore = database.createObjectStore(
+          'notes_os', { keyPath: 'id', autoIncrement: true }
+        );
+
+        objectStore.createIndex('title', 'title', { unique: false })
+        objectStore.createIndex('body', 'body', { unique: false })
+      }
+
+      request.onerror = function (e: any) {
+        console.log('error: ', request.error)
+      }
+    }
+    else {
+      alert("No support for indexedDB")
+    }
+  }, [])
+
   return (
     <>
       <h2>New Note</h2>
