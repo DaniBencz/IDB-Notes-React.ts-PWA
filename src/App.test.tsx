0817c5ed53f88be10c301dc0ced47bdde63d8cf2
Daniel <daniel.bencz@gmail.com>
2020-12-02 10:39:30 +0100

    few simple tests work

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import * as ReactDOM from 'react-dom'
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// as clarification, below code should be same as testing library render function,
+// however, it don't seem to work
+// import * as ReactDOM from 'react-dom'
+// import { getQueriesForElement } from '@testing-library/react';
+/* const render = (component: JSX.Element) => {
+	const root = document.createElement('div')
+	ReactDOM.render(component, root)
+	return getQueriesForElement(root)	// has getByText and getByLabelText
+} */
+
+window.alert = jest.fn()	// to supperess 'Error: Not implemented: window.alert'
+
+test('renders Waiting for DB', () => {
+	// optianlly cleaning up effects of mocking on the global object
+	// that otherwise would persist across tests:
+	window.alert.mockClear();
+
 	const { getByText } = render(<App test={false} />);
 	const Waiting = getByText("Waiting for Database Connection...");
 	expect(Waiting).toBeInTheDocument();
 });
 
-// how to mock the state of the component?
-test('renders', () => {
-	const root = document.createElement('div')
-	ReactDOM.render(<App test={true} />, root)
-	expect(root.querySelector('h1').textContent).toBe("IDB Notes")
+test('renders Notes', () => {
+	const { getByText } = render(<App test={true} />);
+	const Notes = getByText("IDB Notes")
+	expect(Notes).toBeInTheDocument()
+
+	// const root = document.createElement('div')
+	// ReactDOM.render(<App test={true} />, root)
+	// expect(root.querySelector('h1').textContent).toBe("IDB Notes")
 })
 
-test('Fake 1', () => {
-	const num = 3
-	expect(true).toBeTruthy()
-	expect(num).toBe(3)
-})
+// test('saves note', () => {
+// 	const { getByText } = render(<App test={true} />);
+// })
